abec64c61035106e81af9eb31dabafb0a2baf236
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-02-19 16:20:17 +0530

    added solution for Merging two sorted list

---
+++ solution2.py
@@ -0,0 +1,20 @@
+# Solution : 1
+
+a = [3, 4, 6, 10, 11, 18]
+b = [1, 5, 7, 12, 13, 19, 21]
+c = []
+while a and b:
+    if a[0] < b[0]:
+        c.append(a.pop(0))
+    else:
+        c.append(b.pop(0))
+        
+# either a or b can be not empty
+print(c + a + b)
+
+
+# Solution : 2 in pythonic way
+
+a.extend(b)
+c = sorted(a)
+print(c)
