f91337f95c3082234523f746501115cbafc567c3
asmitamahamuni <asmitapmahamuni@gmail.com>
2018-02-19 16:06:04 +0530

    added solution for problem : find list of anagrams from a list of words

--- solution1.py
+++ solution1.py
@@ -2,8 +2,6 @@ anagram = []
 words = ['fired', 'alert', 'remain', 'alter', 'allergy', 'gallery',
          'abets', 'baste', 'fried', 'beast', 'beats']
 
-# construct a words_dict = { key:value } = { key:sorted(key),...}
-# (ex) { 'fired':'deirf', 'alert':'aelrt',...}
 words_dict = {}
 for w in words:
    words_dict[w] = ''.join(sorted(w))
@@ -11,10 +9,6 @@ for w in words:
 # make a list of groups with the same value
 new_words = sorted(words_dict.values())
 
-# make a new dic with sorted keys
-# (ex)
-#  {'aegllry': ['allergy', 'gallery'],
-#   'defir': ['fried', 'fired'], ..........}
 anagram = {}
 for s in set(new_words):
    anagram[s]=[]
@@ -22,11 +16,6 @@ for s in set(new_words):
       if s == v:
          anagram[s].append(k) 
    
-# make list of anagrams
-# (ex) 
-# [['allergy', 'gallery'], 
-#  ['abets', 'baste', 'beast', 'beats'],
-#  ['fried', 'fired'], ['remain'], ['alter', 'alert']]
 anagram_list = []
 for k,v in anagram.items():
    anagram_list.append(v)
