d98a4dfbc470318bbd1306034ce1e247ed7ab753
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-02-20 14:31:12 +0530

    Examples for map, filter and reduce

---
+++ 5_solution.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+l = [x for x in range(10) if x % 2 == 0]
+print(l)
+
+m = list(filter(lambda x:x % 2 == 0, [x for x in range(10)] ))
+print(m)
+
+o = list(map(lambda x: x**2, m))
+print(o)
+
+p = reduce(lambda x,y:x+y, o)
+print(p)
