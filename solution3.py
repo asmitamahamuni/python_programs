2a468c7dd4c7965faa96a01f3a9d3caa47ef3e65
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-02-20 11:06:16 +0530

    Create solution3.py

---
+++ solution3.py
@@ -0,0 +1,10 @@
+ss = """I figured it out
+I figured it out from black and white
+Seconds and hours
+Maybe they had to take some time"""
+
+words = ss.split()
+d = {}.fromkeys(words,0)
+for w in words:
+    d[w] += 1
+print(d)
