273e82e3da4cf8aeab147c5f4efba364a9e5ce94
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-02-20 11:10:36 +0530

    Update solution3.py

--- solution3.py
+++ solution3.py
@@ -3,8 +3,27 @@ I figured it out from black and white
 Seconds and hours
 Maybe they had to take some time"""
 
+# Solution 1
 words = ss.split()
 d = {}.fromkeys(words,0)
 for w in words:
     d[w] += 1
 print(d)
+
+
+# Solution 2
+d = {}
+for w in ss.split():
+    d[w] = d.get(w,0) + 1
+print(d)
+
+
+# Solution 3 
+from collections import defaultdict
+d = defaultdict(int)
+for w in words:
+    d[w] += 1
+print(d)
+
+
+
