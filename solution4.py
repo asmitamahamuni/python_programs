572ea8fe0560456728423a37acb870e3605271be
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-02-20 14:24:49 +0530

    Update solution4.py

--- solution4.py
+++ solution4.py
@@ -16,3 +16,13 @@ d2 = {}
 for k,v in cities.items():
        d2.setdefault(v,[]).append(k)
 print(d2)
+
+#Another example of creating dict from list
+L = [1,2,4,8,16,32,64,128,256,512,1024,32768,65536,4294967296]
+
+from collections import defaultdict
+d = defaultdict(list)
+for i in L:
+    d[len(str(i))].append(i)
+print(d)
+print({k:v for k,v in d.items()})
