1e1d728ab87f0a71b9759539da94136186a690e1
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-02-28 12:08:38 +0530

    Reversing a string - Recursive

---
+++ string_reverse_recursive.py
@@ -0,0 +1,11 @@
+# Reversing a string - Recursive
+
+def reverse(input):
+    print input
+    if len(input) <= 1:
+	    return input
+	
+    return reverse(input[1:]) + input[0]
+
+s = 'reverse'	
+print(reverse(s))
