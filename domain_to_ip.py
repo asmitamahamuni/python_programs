63f1f7b493471cf265d3e37020ebd656d7220534
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-02-28 19:35:24 +0530

    Converting domain to ip using subprocess

---
+++ domain_to_ip.py
@@ -0,0 +1,14 @@
+# Converting domain to ip
+
+import subprocess
+
+def getIPs(file):
+   ips = {}
+   with open(file) as f:
+      for line in f:
+         domain = line.strip()
+         out = subprocess.check_output(["dig","+noall","+answer", domain])
+         ips[domain] = out.split()[4]
+   return ips
+
+print(getIPs('domains.txt'))
