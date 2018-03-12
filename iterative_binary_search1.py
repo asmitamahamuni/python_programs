20d343c8a83926b84cf12ade442f90d3fbafeb40
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2018-03-12 17:39:22 +0530

    iterative binary search

---
+++ iterative_binary_search1.py
@@ -0,0 +1,32 @@
+# returns True if found the item
+# otherwise, returns False
+
+def bSearch(a, item):
+   st = 0
+   end = len(a)-1
+   mid = len(a)/2
+   left = st
+   right = end
+   while True:
+      # found
+      if a[mid] == item:
+         return True
+      # upper
+      elif a[mid] < item:
+         left = mid + 1
+      # lower
+      else:
+         right = mid - 1
+
+      mid = (left+right)/2
+      if mid < st or mid > end: break
+      if left == right and a[mid] != item: break
+   return False
+
+if __name__ =="__main__":
+   a = [1,3,4,5,8,9,17,22,36,40]
+   print(bSearch(a, 9))
+   print(bSearch(a, 10))
+   print(bSearch(a, 36))
+   print(bSearch(a, 5))
+   print(bSearch(a, 22))
