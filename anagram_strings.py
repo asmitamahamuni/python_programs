a0c4cf4d8231fb6f271bc0dded620cd77e570476
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2021-02-20 17:34:35 +0530

    Update anagram_strings.py

--- anagram_strings.py
+++ anagram_strings.py
@@ -8,11 +8,8 @@ words_dict = {}
 for w in words:
    words_dict[w] = ''.join(sorted(w))
 
-# make a list of groups with the same value
-new_words = sorted(words_dict.values())
-
 anagram = {}
-for s in set(new_words):
+for s in set(words_dict.values()):
    anagram[s]=[]
    for k,v in words_dict.items():
       if s == v:
