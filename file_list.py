5e4b33a1fc71392b75406ab171e268dc41181490
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2021-02-20 23:12:05 +0530

    Update file_list.py

--- file_list.py
+++ file_list.py
@@ -1,12 +1,10 @@
 # Print list of files and directories
-
 import os
 
 def file_list(dir):
-    basedir = dir
     subdir_list = []
     for item in os.listdir(dir):
-        fullpath = os.path.join(basedir,item)
+        fullpath = os.path.join(dir,item)
         if os.path.isdir(fullpath):
             subdir_list.append(fullpath)
         else:
@@ -15,4 +13,4 @@ def file_list(dir):
     for d in subdir_list:
         file_list(d)
 
-file_list('/dir')
+file_list('D:\Workspace\test\PythonProject')
