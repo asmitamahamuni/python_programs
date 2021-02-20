aa8880054fdb9f5cb5c7117fe58df6030ea05f08
Asmita Mahamuni <asmitapmahamuni@gmail.com>
2021-02-20 17:48:21 +0530

    Code refactoring
    Code refactoring

--- check_anagram_string.py
+++ check_anagram_string.py
@@ -1,8 +1,8 @@
 def isAnagram(w1, w2):
-  if (sorted(list(w1)) != sorted(list(w2))):
+  if sorted(w1) != sorted(w2):
     return False
   return True
 
 words = [("evil","vile"), ("pat","tap"),("mile","nile")]
 for w in words:
-  print(w,isAnagram(w[0],w[1]))  
+  print(w, isAnagram(w[0],w[1]))  
